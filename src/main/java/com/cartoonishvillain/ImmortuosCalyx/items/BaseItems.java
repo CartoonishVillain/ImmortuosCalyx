21ba3b2669fe2bb15deaa52f72651615a6c8466e
Cartoonish Villain <jedijoe93@gmail.com>
2022-01-25 22:05:10 -0500

    Adds new items, converts lore to infinite system.

--- src/main/java/com/cartoonishvillain/ImmortuosCalyx/items/BaseItems.java
+++ src/main/java/com/cartoonishvillain/ImmortuosCalyx/items/BaseItems.java
@@ -8,34 +8,22 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BaseItems extends Item{
-    String lore1;
-    String lore2;
-    String lore3;
-    String lore4;
-    public BaseItems(Item.Properties properties, String lore1, String lore2, String lore3, String lore4) {
+
+    ArrayList<String> listOfLore;
+    public BaseItems(Item.Properties properties, String... lore) {
         super(properties);
-        this.lore1 = lore1;
-        this.lore2 = lore2;
-        this.lore3 = lore3;
-        this.lore4 = lore4;
+        listOfLore = new ArrayList<>();
+        listOfLore.addAll(List.of(lore));
     }
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
-        if(lore1 != ""){
-            tooltip.add(new TextComponent(lore1));
-            if(lore2 != ""){
-                tooltip.add(new TextComponent(lore2));
-                if(lore3 != ""){
-                    tooltip.add(new TextComponent(lore3));
-                    if(lore4 != ""){
-                        tooltip.add(new TextComponent(lore4));
-                    }
-                }
-            }
+        for(String loreItem : listOfLore) {
+            tooltip.add(new TextComponent(loreItem));
         }
     }
 }
