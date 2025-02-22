a74d37a309b2eb288bd792bc0c8fdf8cd812ff02
Cartoonish Villain <6913677+CartoonishVillain@users.noreply.github.com>
2025-02-22 17:51:08 -0500

    Add localization for gene encoder, genetic researcher, and the gene effects

--- common/src/main/java/com/cartoonishvillain/immortuoscalyx/Constants.java
+++ common/src/main/java/com/cartoonishvillain/immortuoscalyx/Constants.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Constants {
-
 	public static final String MOD_ID = "immortuoscalyx";
 	public static final String MOD_NAME = "Immortuos Calyx";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
